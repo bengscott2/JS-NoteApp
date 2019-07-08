9b7b419a0c56877efd2143745c934005a07457ee
Ben Scott <bengscott2@gmail.com>
2019-07-08 16:55:57 +0100

    add Note model

--- spec/note-modelSpec.js
+++ spec/note-modelSpec.js
@@ -1,6 +1,14 @@
 function testNoteModelObjectExists() {
   var note = new Note();
-  assert.isTrue(typeof Object === typeof note, "Test note model object exists")
+  var object = new Object();
+  assert.isTrue(typeof object === typeof note, "Test note model object exists")
 };
 
 testNoteModelObjectExists();
+
+function testNoteAcceptsString() {
+  var note = new Note('My favourite language is JavaScript');
+  assert.isTrue('My favourite language is JavaScript' === note.displayNote(), 'Test note model accepts string')
+};
+
+testNoteAcceptsString();
--- src/note-model.js
+++ src/note-model.js
@@ -1,2 +1,12 @@
-function Note() {
-  };
+(function (exports) {
+
+  var Note = function (text) {
+    this.text = text
+  }
+
+  Note.prototype.displayNote = function () {
+    return this.text
+  }
+
+  exports.Note = Note
+})(this);
