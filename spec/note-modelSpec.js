17a9d4ae7b300a766c6430ba9741c2dff1c04632
Chris Groves <chris_groves87@hotmail.co.uk>
2019-07-08 15:30:01 +0100

    amend testing file

--- spec/note-modelSpec.js
+++ spec/note-modelSpec.js
@@ -1,6 +1,6 @@
 function testNoteModelObjectExists() {
   var note = new Note();
-  assert.isTrue(typeof Object === typeof Note, "Test note model object exists")
+  assert.isTrue(typeof Object === typeof note, "Test note model object exists")
 };
 
 testNoteModelObjectExists();
