1d32cb5d78e483a44bfed813d0819c6f1c5993b5
Ben Scott <bengscott2@gmail.com>
2019-07-10 16:07:20 +0100

    add test for printHtml function in NoteListView Object

--- spec/note-controllerSpec.js
+++ spec/note-controllerSpec.js
@@ -10,17 +10,29 @@ NoteListViewDouble.prototype.printHtml = function () {
   return '<ul><li><div>Favourite food: pesto</div></li></ul>'
 };
 
-(function () {
-  function testFunctionTakesANoteList () {
-    var noteListDouble = new NoteListViewDouble
-    var object = new Object
-    var docID = document.createElement('app')
-    var testName = 'test existance of NoteController function'
-    noteController = new NoteController (notelist, NoteListViewDouble, docID)
-    assert.isTrue(typeof noteController === typeof object, testName)
-  }
+
+(function testFunctionTakesANoteList () {
+  var noteListViewDouble = NoteListViewDouble
+  var noteListDouble = new OneNoteListDouble()
+  var object = new Object
+  var testName = 'test existance of NoteController function'
+  noteController = new NoteController (noteListDouble, noteListViewDouble)
+
+  assert.isTrue(typeof noteController === typeof object, testName)
 })();
 
-// (function () {
-//   function testInsertHtmlFunction ()
-// })
+
+(function testInsertHtmlFunction () {
+  element = document.createElement('div')
+  element.setAttribute('id','app')
+  document.getElementById('test').appendChild(element)
+
+  var noteListDouble = new OneNoteListDouble()
+  var noteListViewDouble = NoteListViewDouble
+  var testName = 'Test insertHtml function of NoteController chages app element on page'
+  noteController = new NoteController (noteListDouble, NoteListViewDouble)
+  noteController.insertHtml();
+  var innerHtml = document.getElementById('app').innerHTML
+
+  assert.isTrue(innerHtml === '<ul><li><div>Favourite food: pesto</div></li></ul>', testName)
+})();
